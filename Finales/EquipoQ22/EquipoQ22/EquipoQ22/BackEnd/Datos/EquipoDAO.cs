f84ae7b687d61c5edfedc2f0388c062904055ac8
DIEGO_GUIZZO <dguizzo@gmail.com>
2024-02-13 14:59:09 -0300

    correcionDAO

--- Finales/EquipoQ22/EquipoQ22/EquipoQ22/BackEnd/Datos/EquipoDAO.cs
+++ Finales/EquipoQ22/EquipoQ22/EquipoQ22/BackEnd/Datos/EquipoDAO.cs
@@ -28,70 +28,14 @@ namespace EquipoQ22.BackEnd.Datos
 
             return lst;
         }
-        public bool Crear(Equipo oEquipo)
-         {
-            bool resultado = true;
-            SqlConnection cnn = HelperDB.ObtenerInstance().RecuperarConexión();
-            SqlTransaction t = null;
-
-            try
-            {
-                cnn.Open();
-                SqlCommand cmd = new SqlCommand();
-                t = cnn.BeginTransaction();
-                cmd.CommandText = "SP_INSERTAR_EQUIPO";
-                cmd.Connection = cnn;
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Transaction = t;
-
-                cmd.Parameters.Clear();
-
-                cmd.Parameters.AddWithValue("@pais", oEquipo.Pais);
-                cmd.Parameters.AddWithValue("@director_tecnico", oEquipo.Director);
-
-                SqlParameter pout = new SqlParameter();
-                pout.ParameterName = "@id";
-                pout.DbType = DbType.Int32;
-                pout.Direction = ParameterDirection.Output;
-                cmd.Parameters.Add(pout);
-
-                cmd.ExecuteNonQuery();
-                int nroEquipo = (int) pout.Value;
-
-                SqlCommand cmd2;
-
-                foreach (Jugador j in oEquipo.DetalleEquipo) {
-                    cmd2 = new SqlCommand();
-                    cmd2.CommandText = "SP_INSERTAR_DETALLES_EQUIPO";
-                    cmd2.Connection = cnn;  
-                    cmd2.Transaction = t;
-                    cmd2.CommandType = CommandType.StoredProcedure;
-                    cmd2.Parameters.AddWithValue("@id_equipo", nroEquipo);
-                    cmd2.Parameters.AddWithValue("@id_persona", j.Persona.Id);
-                    cmd2.Parameters.AddWithValue("@camiseta", j.Camiseta);
-                    cmd2.Parameters.AddWithValue("@posicion", j.Posicion);
-                    cmd2.ExecuteNonQuery();
-
-                }
 
 
-                t.Commit();
-            }
-            catch (Exception ex)
-            {
-                if (t != null) { t.Rollback(); }
-            }
 
+        public bool Crear( Equipo oEquipo) {
 
-            finally
-            {
-                if (cnn != null && cnn.State == ConnectionState.Open) { cnn.Close(); }
-            }
-
-            return resultado;
-
-
-         }
+            return HelperDB.ObtenerInstance().InsertSQL("SP_INSERTAR_EQUIPO", "INSERTAR_DETALLE_EQUIPO",oEquipo);
+        }
+        
 
     }
 }
--- Finales/EquipoQ22/EquipoQ22/EquipoQ22/BackEnd/Datos/HelperDB.cs
+++ Finales/EquipoQ22/EquipoQ22/EquipoQ22/BackEnd/Datos/HelperDB.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EquipoQ22.BackEnd.Dominio;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -56,7 +57,71 @@ namespace EquipoQ22.BackEnd.Datos
             return dt;
         }
 
+        public bool InsertSQL(string spMaestro,string spDetalle, Equipo oEquipo)
+        {
+            bool resultado = true;
+            
+            SqlTransaction t = null;
 
+            try
+            {
+                cnn.Open();
+                SqlCommand cmd = new SqlCommand();
+                t = cnn.BeginTransaction();
+                cmd.CommandText = spMaestro;
+                cmd.Connection = cnn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Transaction = t;
+
+                cmd.Parameters.Clear();
+
+                cmd.Parameters.AddWithValue("@pais", oEquipo.Pais);
+                cmd.Parameters.AddWithValue("@director_tecnico", oEquipo.Director);
+
+                SqlParameter pout = new SqlParameter();
+                pout.ParameterName = "@id";
+                pout.DbType = DbType.Int32;
+                pout.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(pout);
+
+                cmd.ExecuteNonQuery();
+                int nroEquipo = (int)pout.Value;
+
+                SqlCommand cmd2;
+
+                foreach (Jugador j in oEquipo.DetalleEquipo)
+                {
+                    cmd2 = new SqlCommand();
+                    cmd2.CommandText = spDetalle;
+                    cmd2.Connection = cnn;
+                    cmd2.Transaction = t;
+                    cmd2.CommandType = CommandType.StoredProcedure;
+                    cmd2.Parameters.AddWithValue("@id_equipo", nroEquipo);
+                    cmd2.Parameters.AddWithValue("@id_persona", j.Persona.Id);
+                    cmd2.Parameters.AddWithValue("@camiseta", j.Camiseta);
+                    cmd2.Parameters.AddWithValue("@posicion", j.Posicion);
+                    cmd2.ExecuteNonQuery();
+
+                }
+
+
+                t.Commit();
+            }
+            catch (Exception ex)
+            {
+                if (t != null) { t.Rollback(); }
+            }
+
+
+            finally
+            {
+                if (cnn != null && cnn.State == ConnectionState.Open) { cnn.Close(); }
+            }
+
+            return resultado;
+
+
+        }
         //public bool Delete_SQL(string spNombre, List<Parametro> valores)
         //{
         //    bool resultado = false;
@@ -87,7 +152,7 @@ namespace EquipoQ22.BackEnd.Datos
         //    {
         //        if (t != null) {  t.Rollback(); }
         //    }
-            
+
 
         //    finally
         //    { 
